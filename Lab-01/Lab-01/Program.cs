743b2031a7b1d2f051f9a0296a81c0b0ec7285f1
emtu5 <paulaesthetical@gmail.com>
2023-10-13 18:15:18 +0300

    initial commit

---
+++ Lab-01/Lab-01/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab_01;
+using System.Security.Cryptography.X509Certificates;
+
+var materie1 = new Materie
+{
+    Denumire = "Algoritmi fundamentali",
+    Durata = "2h curs, 2h seminar/lab"
+};
+
+var materie2 = new Materie
+{
+    Denumire = "Dezvoltare Aplicatii Web",
+    Durata = "2h curs, 2h lab"
+};
+
+var materie3 = new Materie
+{
+    Denumire = "Dezvoltare Aplicatii Web",
+    Durata = "2h curs, 2h lab"
+};
+
+var student1 = new Student
+{
+    Nume = "Ionescu",
+    Prenume = "Alex",
+    CNP = "5010101170018",
+    Materii = new List<Materie> { materie1, materie2 },
+    NrMatricol = "195/2022"
+};
+
+var student2 = new Student
+{
+    Nume = "Bobiță",
+    Prenume = "Andrei",
+    CNP = "5020202450019",
+    Materii = new List<Materie> { materie1, materie3 },
+    NrMatricol = "34/2022"
+};
+
+var student3 = new Student
+{
+    Nume = "Tedi",
+    Prenume = "Zamfir",
+    CNP = "5030303230019",
+    Materii = new List<Materie> { materie1, materie2, materie3 },
+    NrMatricol = "367/2022"
+};
+
+foreach (Materie subject in student2.Materii)
+{
+    Console.WriteLine(subject.Denumire + ": " + subject.Durata);
+}
+
+foreach (Student student in new List<Student> { student1, student2, student3 })
+{
+    Console.WriteLine(student.NrMatricol);
+}
---
+++ Lab-01/Lab-01/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01
+{
+    public class Student
+    {
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public string CNP { get; set; }
+        public string NrMatricol { get; set; }
+        public List <Materie> Materii { get; set; }
+    }
+}
